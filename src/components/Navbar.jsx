39e0e81e720a12b51e7ff7afbfd3c229740c1305
Anton Rosh <rosh8442@gmail.com>
2023-05-29 23:03:54 -0700

    update hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-10 bg-[#0a172f] text-white ">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a172f] text-white ">
+      {/* Logo */}
       <div>
         <img
-          className="object-contain"
+          className="object-contain left-4"
           src={Logo}
           alt="logo"
           style={{ width: "150px" }}
         />
       </div>
       {/* Menu icons */}
-      <div>
-        <ul className="hidden md:flex">
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>SKILLS</li>
-          <li>WORK</li>
-          <li>CONTACTS</li>
-        </ul>
-      </div>
+      <ul className="hidden md:flex">
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>SKILLS</li>
+        <li>WORK</li>
+        <li>CONTACTS</li>
+      </ul>
       {/* Hamburger menu */}
-      <div className="hidden">
-        <FaBars />
+      <div
+        onClick={handleClick}
+        className="flex md:hidden z-10 absolute right-4 cursor-pointer"
+      >
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* Mobile menu */}
-      <div className="hidden">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contacts</li>
-        </ul>
-      </div>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a172f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">HOME</li>
+        <li className="py-6 text-4xl">ABOUT</li>
+        <li className="py-6 text-4xl">SKILLS</li>
+        <li className="py-6 text-4xl">WORK</li>
+        <li className="py-6 text-4xl">CONTACTS</li>
+      </ul>
       {/* Social icons */}
       <div></div>
     </div>
