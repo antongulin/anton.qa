32c9e58f295ceb97489f70eaffd4bbf9cb618647
Anton Rosh <rosh8442@gmail.com>
2023-05-31 20:36:26 -0700

    add About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a172f] text-[#f2f3f5]">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-white">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-3xl font-bold">
+            <p>Hello and welcome! Feel free to browse through the site.</p>
+          </div>
+          <div>
+            <p>
+              With a solid foundation in programming languages such as
+              TypeScript, Java, C#, and Python, I'm skilled in utilizing testing
+              tools like Playwright and Selenium WebDriver. Furthermore, I'm
+              experienced in working with testing frameworks such as TestNG,
+              JUnit, NUnit, PyTest and others.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
