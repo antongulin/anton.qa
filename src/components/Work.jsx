5f94607fb663567f3a2ee4c7a2f7f62d492d6539
Anton Rosh <rosh8442@gmail.com>
2023-06-02 19:51:43 -0700

    optimize Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import qrCodeGenerator from "../assets/projects/qrCodeGenerator.png";
-import personalPortfolio from "../assets/projects/personalPortfolio.png";
-import pythonPlaywright from "../assets/projects/pythonPlaywright.png";
-import javaSelenium from "../assets/projects/javaSelenium.png";
-import typescriptPlaywright from "../assets/projects/typescriptPlaywright.jpg";
-import openAiNodeJs from "../assets/projects/openAiNodeJs.png";
+import { data } from "../data/works.js";
 
 const Work = () => {
+  const project = data;
   return (
     <div name="work" className="w-full md:h-screen text-[#f2f3f5] bg-[#0a172f]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
@@ -17,193 +13,36 @@ const Work = () => {
           <p className="py-6">Presenting my recent projects and code</p>
         </div>
 
-        {/* Container */}
+        {/* Container for projects */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${qrCodeGenerator})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                QR Code Generator
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://antonrosh.dev/qr-code-component/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/qr-code-component"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+          {/* Gird Item */}
+          {project.map((project, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${project.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effect for images */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  {project.name}
+                </span>
+                <div className="pt-8 text-center">
+                  <a href={project.github} target="_blank" rel="noreferrer">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+
+                  <a href={project.demo} target="_blank" rel="noreferrer">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Live
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${personalPortfolio})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Personal Portfolio
-              </span>
-              <div className="pt-8 text-center">
-                <a href="http://antonrosh.dev" target="_blank" rel="noreferrer">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/antonrosh.github.io"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${pythonPlaywright})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Python + Playwright
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://github.com/antonrosh/disney-python-playwright/actions/workflows/test.yml"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/disney-python-playwright"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${javaSelenium})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Java + Selenium
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://www.youtube.com/watch?v=XaUJovUyjj4"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/NathanJamesWorkSample"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${typescriptPlaywright})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                TypeScript + Playwright
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://github.com/antonrosh/typescript-playwright"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Work In Progress
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/typescript-playwright"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${openAiNodeJs})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Open AI + NodeJS
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://github.com/antonrosh/openai-nodejs"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Work In Progress
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/antonrosh/openai-nodejs"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
