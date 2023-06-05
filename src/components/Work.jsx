9c87987619a713daaab52ea66433a0c843540811
Anton Rosh <rosh8442@gmail.com>
2023-06-04 17:04:31 -0700

    update button names

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -30,13 +30,13 @@ const Work = () => {
                 <div className="pt-8 text-center">
                   <a href={project.github} target="_blank" rel="noreferrer">
                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Code
+                      Live
                     </button>
                   </a>
 
                   <a href={project.demo} target="_blank" rel="noreferrer">
                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Live
+                      Code
                     </button>
                   </a>
                 </div>
